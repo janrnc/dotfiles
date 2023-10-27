d51f9c35a6f93c430feddf7e0f7223a5f78a7451
Jan Franchini <janfranchini@gmail.com>
2023-10-27 22:21:19 +0200

    feat(pdftk): add merge command

---
+++ pdftk/tests/merge-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m pdftk -p merge [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "merge"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
